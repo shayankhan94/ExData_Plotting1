8723e4c1d2c9f0b6fca4b8bf6617729644e7d82c
shayankhan94 <shayanjawed@outlook.com>
2014-07-13 22:42:16 +0500

    plot1.R
    this file can just be sourced, for e.g, source("plot1.R") and after this you can find the plot1.png in your working directory.

---
+++ plot1.R
@@ -0,0 +1,5 @@
+source("download_prepare_data.R")
+data=prepareData()
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)",col="red")
+dev.off()
