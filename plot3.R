7cfbde9ddba90c1b9cec5bfe72fb3df7d7488725
shayankhan94 <shayanjawed@outlook.com>
2014-07-13 22:46:52 +0500

    plot3.R
    this file can just be sourced, for e.g, source("plot3.R") and after this you can find the plot3.png in your working directory.

---
+++ plot3.R
@@ -0,0 +1,8 @@
+source("download_prepare_data.R")
+data=prepareData()
+png(filename = "plot3.png", width = 480, height = 480, units = "px")
+plot(data$DateTime, data$Sub_metering_1, type='l',xlab='', ylab='Energy sub metering')
+lines(data$DateTime, data$Sub_metering_2, col='red')
+lines(data$DateTime, data$Sub_metering_3, col='blue')
+legend('topright',legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),col=c('black', 'red', 'blue'),lty='solid')
+dev.off()
